83605d10af391c4ca1b0c5a2ae527bac5772a60f
Luis Medina <vluismedina@gmail.com>
2021-06-15 22:04:31 -0400

    primera clase

---
+++ claseUno.js
@@ -0,0 +1,21 @@
+// Comentario de una linea
+
+/*Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam reprehenderit tempore deleniti accusamus perferendis ex omnis et adipisci cum excepturi a laudantium cupiditate, temporibus optio porro, eligendi sequi amet repellat!*/
+
+// const miNombre = "Luis";
+// const apellido = "Medina";
+// const numero = 31;
+// const espacio = " ";
+
+// const concatenacion = miNombre + " " + apellido + " " + numero;
+// console.log(concatenacion);
+
+const nombre = prompt("Por favor ingrese su Nombre");
+const apellido = prompt("Por favor ingrese su Apellido")
+let edad = parseInt(prompt("Ingrese su Edad"));
+const localidad = prompt("Ingrese su Pais");
+let resultado = nombre + " " + apellido + " " + edad + " " + localidad;
+alert(resultado);
+console.log(resultado);
+
+
